cd463d83323fc8f4778cd3b733807cb1acd3c709
Martin Finkel <finkel2804@gmail.com>
2020-06-27 20:38:22 +0200

    test: add long path test project

---
+++ src/Assets/TestProjects/LongPathProject/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/thisisaveryveryverylongpathanditkeepsgoing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace thisisaveryveryverylongpathanditkeepsgoing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
